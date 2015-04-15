37efc7c9874a8afa6b9fa27ffff7ad1d1cdcb7cf
Marcos Wright-Kuhns <marcoswk@haikulearning.com>
2015-04-15 10:26:24 -0700

    Reduce ABC of ToLoad#to_a based on Rubocop
    The method is still just a little bit long, but at least it's a bit better
    
    See http://c2.com/cgi/wiki?AbcMetric

--- lib/dotenv/to_load.rb
+++ lib/dotenv/to_load.rb
@@ -13,12 +13,7 @@ module Dotenv
     def to_a
       to_load = []
 
-      unless supports_inflection(app_env)
-        fail InvalidAppEnv, <<-FAIL.gsub(/^\s+|\n/, "")
-          The `app_env` must support StringInquirer methods
-          (like `#production?` or `#development?`) #{app_env.inspect}"
-        FAIL
-      end
+      validate_app_env
 
       # Dotenv values for your local development environment only
       to_load << File.join(app_root, ".env.custom") if app_env.development?
@@ -28,16 +23,8 @@ module Dotenv
 
       # Dotenv values specific to the current Rails.env
       app_env_dotenv = File.join(app_root, ".env.#{Rails.env}")
-
-      if File.exist?(app_env_dotenv) && File.size(app_env_dotenv) > 0
-        to_load << app_env_dotenv
-      elsif !app_env.development? && !app_env.test?
-        # If this file does *NOT* exist, then something has gone terribly wrong
-        fail MissingDotenv, <<-FAIL.gsub(/^\s+/, "")
-          The #{app_env_dotenv} file does not exist!
-          This is often a sign of a failed/misconfigured symlink.
-        FAIL
-      end
+      protect_against_missing_app_env_dotenv(app_env_dotenv)
+      to_load << app_env_dotenv
 
       # Last, but not least, the good 'ol .env file
       to_load << File.join(app_root, ".env")
@@ -58,6 +45,26 @@ module Dotenv
     end
 
     private
+
+    def validate_app_env
+      return true if supports_inflection(app_env)
+
+      fail InvalidAppEnv, <<-FAIL.gsub(/^\s+|\n/, "")
+        The `app_env` must support StringInquirer methods
+        (like `#production?` or `#development?`) #{app_env.inspect}"
+      FAIL
+    end
+
+    def protect_against_missing_app_env_dotenv(path)
+      return true if File.exist?(path) && File.size(path) > 0
+      return true if app_env.development? || app_env.test?
+
+      # If this file does *NOT* exist, then something has gone terribly wrong
+      fail MissingDotenv, <<-FAIL.gsub(/^\s+/, "")
+        The #{path} file does not exist!
+        This is often a sign of a failed/misconfigured symlink.
+      FAIL
+    end
 
     # Returns true if the given Object responds to the production? method
     def supports_inflection(str)
